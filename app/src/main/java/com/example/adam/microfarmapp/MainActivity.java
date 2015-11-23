3d832e6221522ebedf6d1539ddd5bd175f77c9bb
ajboole <ajboole@gmail.com>
2015-11-23 15:18:30 -0600

    Thinner Buttons

--- app/src/main/java/com/example/adam/microfarmapp/MainActivity.java
+++ app/src/main/java/com/example/adam/microfarmapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.adam.microfarmapp;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         Log.i(TAG, "onCreate");
