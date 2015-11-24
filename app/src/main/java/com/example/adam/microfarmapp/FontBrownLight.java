1b51457a01451bd47049c56dc0cf56e2d33e51d3
ajboole <ajboole@gmail.com>
2015-11-24 09:18:08 -0600

    Added Button Functionality to Home

---
+++ app/src/main/java/com/example/adam/microfarmapp/FontBrownLight.java
@@ -0,0 +1,20 @@
+package com.example.adam.microfarmapp;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Adam on 11/24/2015.
+ */
+public class FontBrownLight extends TextView {
+
+    public FontBrownLight(Context context, AttributeSet attrs){
+
+        super(context, attrs);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/brownlight.ttf"));
+
+    }
+
+}
--- app/src/main/java/com/example/adam/microfarmapp/MainActivity.java
+++ app/src/main/java/com/example/adam/microfarmapp/MainActivity.java
@@ -1,20 +1,17 @@
 package com.example.adam.microfarmapp;
 
-import android.app.ActionBar;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.content.Intent;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -31,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         Log.i(TAG, "onCreate");
+    }
+
+    public void onClickHome(View view){
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
     }
 
     public void onClickLight(View view){
